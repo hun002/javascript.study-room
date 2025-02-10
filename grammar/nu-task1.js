0fc34de1219361ad6d0623110f4e3abbe0c89ee9
joo <consee0425@gmail.com>
2025-02-10 23:11:38 +0900

    second

---
+++ grammar/nu-task1.js
@@ -0,0 +1,61 @@
+// 사진속 값 바꾸기
+
+// 첫번째
+
+// let a = 30
+// let b = 50
+// let temp = 0
+
+// console.log(a,b,temp) 
+
+// 두번째
+
+// let a = 30
+// let b = 50
+// let temp = 30
+// a-=30
+
+// console.log(a,b,temp)
+
+// 세번째
+
+// let a = 30
+// let b = 50
+// let temp = 30
+// a+=20
+// b-=50
+
+// console.log(a,b,temp)
+
+// 네번째
+
+// let a = 30
+// let b = 50
+// let temp = 30
+// a+=20
+// b-=20
+// temp-=30
+
+// console.log(a,b,temp)
+
+// 주어진 문제의 값바꾸기
+// 방법1
+// let a = 1
+// let b = 2 
+// console.log(a,b) // 1,2 
+
+// a++
+// b--
+
+// console.log(a,b) //  2,1 
+
+// 방법2
+// let a = 1
+// let b = 2 
+// console.log(a,b) // 1,2 
+
+// let c = a
+// a = b
+// b = c 
+
+// console.log(a,b) //  2,1 
