d6ea55068c07cf169bd8f92343bcdfa2e3469f14
joo <consee0425@gmail.com>
2025-02-10 22:56:23 +0900

    first javascript task

---
+++ grammar/nu-task2.js
@@ -0,0 +1,46 @@
+// 20 + 30
+// 50
+// “20” + “30”
+// 2030
+// “Hello” + " " + 2021
+// Hello 2021
+// 1 + 2 * 3
+//7
+// (1 + 3) ** 2
+//16
+// 1 / 0
+//1 - 자바스크에선 0으로 나누면 값이 무한대래.
+// 6 % 2
+//0
+// 7.5 % 2
+//1 -1.5
+// 5 == 5
+//ture
+// 5 === 5
+//ture
+// 5 == “5”
+//true
+// 5 === “5”
+//false
+// 5 != 5.0
+//false
+// 5 !== 5.0
+//false
+// “true” === true
+//false
+// 5 <= 5.0
+//true
+// 5 >= 5
+//true
+// true || true
+//ture(또는)ture
+// true || false
+//ture(또는)false
+// true && true
+//ture(둘다)
+// true && false
+//false(둘 중 하나가 false이기 떄문에)
+// !true
+//false
+// !false
+//true
