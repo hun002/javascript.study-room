62249317adc8c343a828282786fb000f8dd4ce08
joo <consee0425@gmail.com>
2025-02-14 15:16:59 +0900

    task5

---
+++ grammar/nu-task5.js
@@ -0,0 +1,51 @@
+// //구구단 만들기~
+// let number = 256;
+
+// for (let i = 1; i <= 9; i++) {
+//     console.log(number * i);
+// }
+
+// let sum = 0;
+// for(let i = 1; i <= 100; i++){
+//     sum += i;
+// }
+
+// console.log(sum)
+
+// for (let i=1; i<=100; i++){
+//     if(i%2 != 0){
+//         console.log(i);
+//     }
+// }
+
+// for (let i=1; i<=100; i+=2){
+// console.log(i);
+// }
+
+// for(let i=1; i<=50; i++){
+//     let stringValue = i.toString();
+//     let result = "";
+//     for(let j= 0; j<stringValue.length; j++){
+//         if (stringValue[j]==="3"|| stringValue[j]==="6" || stringValue[j]==="9"){
+//             result += "짝";
+//         }
+//     }
+    
+//     console.log(result.length>0? result:i);
+// }
+
+// let n = 11
+// let isPrime = true
+
+// if (n ===1){
+//     isPrime = false
+// }else{
+//     for(let i=2;i<n;i++){
+//       if(n % i == 0){
+//         isPrime =  false 
+
+//       }
+//     }
+// }
+
+// console.log(isPrime)
