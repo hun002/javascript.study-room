66389aa5053af952dafb825bbf06b8e3b95082d2
joo <consee0425@gmail.com>
2025-02-21 04:33:02 +0900

    task9-final

--- grammar/nu-task9/nu-task9.js
+++ grammar/nu-task9/nu-task9.js
@@ -5,12 +5,36 @@
 // 끝난 할일은 되돌리기 버튼을 클릭하면 다시 되돌릴 수 있다.
 // 탭을 이용해 아이템들을 상태별로 나누어서 볼 수 있다.
 // 모바일 버전에서도 확인할 수 있는 반응형 웹이다
+// 인풋값 초기화
+// 입력한 할일이 없다면 아이템이 추가 안되게 막기( 버튼을 disable 시키거나 or 버튼 클릭시 할일을 입력해주세요 메세지가 뜬다)
 
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div");
 let taskList = [];
+let mode = 'all';
+let filterList = [];
+let underLine = document.getElementById("under-line");
+
+tabs.forEach((menu)=>
+    menu.addEventListener("click",(e)=> tabsIndicator(e))
+);
+
+function tabsIndicator(e){
+    underLine.style.left = e.currentTarget.offsetLeft +5 + "px";
+    underLine.style.width = e.currentTarget.offsetWidth -10 + "px";
+    underLine.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight -3 + "px";
+}
 
 addButton.addEventListener("click",addTask);
+taskInput.addEventListener("focus",function clear(){
+    taskInput.value = "";
+})
+for(let i = 1; i < tabs.length; i++){
+    tabs[i].addEventListener("click", function(event){
+        filter(event);
+    })
+}
 
 function addTask(){
     let task = {
@@ -18,35 +42,43 @@ function addTask(){
         taskContent: taskInput.value,
         isComplete: false,
     };
+    if(taskInput.value == ""){
+        return alert("할일을 입력해주세요.")
+    }
     taskList.push(task);
     console.log(taskList);
     render();
 }
 
 function render(){
+    let list = [];
+    if(mode === "all"){
+        list = taskList;
+    }else if(mode === "ongoing" || mode === "done"){
+        list = filterList;
+    }
     let resultHTML = "";
-    for(let i = 0; i< taskList.length; i++){
-
-        if(taskList[i].isComplete == true){
+    for(let i = 0; i< list.length; i++){
+        if(list[i].isComplete == true){
             resultHTML += `<div class="task task-done">
-                    <span>${taskList[i].taskContent}</span>
+                    <span>${list[i].taskContent}</span>
                     <div class="button-box">
                         <button class="" onclick="toggleComplete('${taskList[i].id}')">
                         <i class="fa-solid fa-rotate-left return"></i>
                         </button>
-                        <button onclick="deleteTask('${taskList[i].id}')">
+                        <button onclick="deleteTask('${list[i].id}')">
                         <i class="fa-solid fa-trash trash"></i>
                         </button>
                     </div>
                 </div>`
         }else {
             resultHTML += `<div class="task">
-                    <span>${taskList[i].taskContent}</span>
+                    <span>${list[i].taskContent}</span>
                     <div class="button-box">
-                        <button onclick="toggleComplete('${taskList[i].id}')">
+                        <button onclick="toggleComplete('${list[i].id}')">
                         <i class="fa-solid fa-check check"></i>
                         </button>
-                        <button onclick="deleteTask('${taskList[i].id}')">
+                        <button onclick="deleteTask('${list[i].id}')">
                         <i class="fa-solid fa-trash trash"></i>
                         </button>
                     </div>
@@ -66,11 +98,33 @@ function toggleComplete(id){
         }
         console.log(taskList);
     }
-    render();
+    filter({target: {id: mode}});
 }
 
 function randomIDGenerate(){
     return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+function filter(event){
+    mode = event.target.id;
+    filterList = [];
+    if(mode === "all"){
+        render();
+    }else if(mode === "ongoing"){
+        for(let i =0; i<taskList.length; i++){
+            if(taskList[i].isComplete === false){
+                filterList.push(taskList[i]);
+            }
+        }
+        render();
+    }else if(mode === "done"){
+        for(let i =0; i<taskList.length; i++){
+            if(taskList[i].isComplete === true){
+                filterList.push(taskList[i]);
+            }
+        }
+        render();
+    }
 }
 
 function deleteTask(id){
@@ -81,5 +135,5 @@ function deleteTask(id){
         }
         console.log(taskList);
     }
-    render();
+    filter({target: {id: mode}});
 }
