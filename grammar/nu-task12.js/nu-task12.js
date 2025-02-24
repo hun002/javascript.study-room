27d3726cc27c40b799ccf8ac11f282fec27e0a21
joo <consee0425@gmail.com>
2025-02-24 19:31:08 +0900

    task12

--- grammar/nu-task12.js/nu-task12.js
+++ grammar/nu-task12.js/nu-task12.js
@@ -1,7 +1,7 @@
 const API_KEY = `57129f0ea2cf4bee82db8ac01e8e46fd`;
 let news = [];
 const getLatestNews = async()=> {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apikey=${API_KEY}`);
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles;
