8ff702bcd7b17a791b4b2084131db4f129144029
joo <consee0425@gmail.com>
2025-02-28 01:52:02 +0900

    taask12

--- grammar/nu-task12/nu-task12.js
+++ grammar/nu-task12/nu-task12.js
@@ -1,38 +1,26 @@
 const API_KEY = `57129f0ea2cf4bee82db8ac01e8e46fd`;
 let newsList = [];
-const getLatestNews = async()=> {
+let category = '';
+
+const menus = document.querySelectorAll(".menus button");
+menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
+
+const InputIn = document.getElementById("myInput");
+const InputBt = document.getElementById("myInput2");
+
+const getLatestNews = async () => {
     const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr`);
     const response = await fetch(url);
     const data = await response.json();
     newsList = data.articles;
     render();
-    console.log("데이터",data);
-    console.log("기사 배열",newsList);
+    console.log("데이터", data);
+    console.log("기사 배열", newsList);
 }
-
+console.log("myInput2:", InputBt);
 getLatestNews();
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-function openInput() {
-    const input = document.getElementById("myInput");
-    const serch = document.getElementById("myInput2");
-    if (input.style.display === "none") {
-        input.style.display = "block";
-        serch.style.display = "block";
-    } else {
-        input.style.display = "none";
-        serch.style.display = "none";
-    }
-}
-
-const render = ()=> {
+const render = () => {
     const newsHTML = newsList.map(
         (news) => `<div class="row news">
                 <div class="col-lg-4 news-img-area">
@@ -56,9 +44,69 @@ const render = ()=> {
 
                 </div>
             </div>`).join('');
-    console.log("ddd",newsHTML);
+    console.log("ddd", newsHTML);
 
-    document.getElementById('news-board').innerHTML=newsHTML;
+    document.getElementById('news-board').innerHTML = newsHTML;
+}
+
+const getNewsByCategory = async (event) => {
+    category = event.target.textContent.toLowerCase();
+    const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${category}`);
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("dad", data);
+    newsList = data.articles;
+    render();
+}
+
+// const getNewsByInput = async () => {
+//     const inputValue = InputIn.value.toLowerCase();
+//     if (!inputValue) return;
+//     if (inputValue == "sports","technology","business","entertainment","science","general","health") {
+//         const url = new URL(`https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&category=${inputValue}`);
+//         console.log("검색어: ", inputValue);
+
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         newsList = data.articles || [];
+//         render();
+//     }
+// }
+
+// InputBt.addEventListener("click", getNewsByInput); //혼자 해본 방법(실행은 돼지만 뭔가 검색어가 한정적)
+
+const getNewsByInput = async () => {
+    const inputValue = InputIn.value;
+    const url = new URL(
+        `https://noona-times-be-5ca9402f90d9.herokuapp.com/top-headlines?country=kr&q=${inputValue}`);
+
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("ket",data);
+    newsList = data.articles || [];
+    render();
+}
+InputBt.addEventListener("click", getNewsByInput);
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function openInput() {
+    const input = document.getElementById("myInput");
+    const serch = document.getElementById("myInput2");
+    if (input.style.display === "none") {
+        input.style.display = "block";
+        serch.style.display = "block";
+    } else {
+        input.style.display = "none";
+        serch.style.display = "none";
+    }
 }
 
 console.log(moment().format('YYYY-MM-DD HH:mm:ss'));
